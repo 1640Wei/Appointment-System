06bc2dc57caf9eb6ecea8cb9e34983285b3436db
Ching-Wei Lai <103228835+1640Wei@users.noreply.github.com>
2023-11-05 07:34:35 -0600

    Create core.h

---
+++ MS1/core.h
@@ -0,0 +1,48 @@
+/*
+*****************************************************************************
+                       Assignment 1 - Milestone 1
+Full Name  : Ching Wei Lai
+Student ID#: 136893211
+Email      : cwlai6@myseneca.ca
+Section    : NHH
+
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+
+
+// Clear the standard input buffer
+void clearInputBuffer(void);
+
+
+// Wait for user to input the "enter" key to continue
+void suspend(void);
+
+
+//test 1
+int inputInt(void);
+
+
+//test 2
+int inputIntPositive(void);
+
+
+//test 3.4
+int inputIntRange(int lowerBound, int upperBound);
+
+
+//test 5
+char inputCharOption(const char charOption[]);
+
+
+//test 6.7.8
+void inputCString(char* string,int minNum, int maxNum );
+
+
+//test 9
+void displayFormattedPhone(const char* phoneNum);
+
